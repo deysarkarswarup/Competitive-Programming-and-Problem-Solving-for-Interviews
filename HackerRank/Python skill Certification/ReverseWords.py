68b2a34e8c8631a18c266fd9acf41661bdeb3d49
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-06-23 09:49:12 +0530

    Create ReverseWords.py

---
+++ HackerRank/Python skill Certification/ReverseWords.py
@@ -0,0 +1,34 @@
+#python: Reverse words and swap cases
+
+    #!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'reverse_words_order_and_swap_cases' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING sentence as parameter.
+#
+
+def reverse_words_order_and_swap_cases(sentence):
+    # Write your code here
+    s=''
+    for i in sentence.split(' '):
+        s+=i[::-1]+' '
+    return s[-2::-1].swapcase()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    sentence = raw_input()
+
+    result = reverse_words_order_and_swap_cases(sentence)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
