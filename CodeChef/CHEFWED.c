26f4a44a2f7e4510d3cd70076e7ae35400df0ba9
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-08-20 00:09:24 +0530

    Update CHEFWED.c

--- CodeChef/CHEFWED.c
+++ CodeChef/CHEFWED.c
@@ -39,4 +39,101 @@ There are N guests (numbered 1 through N) coming to Chef's wedding. Each guest i
 // Example case 2: The optimal solution is to seat all guests at one table. Then, guests 2, 4 and 5 are likely to get into an argument with each other, so the inefficiency is K+3=17.
 
 
-//Solution will be uploaded after contest Ends
+//Solution
+  #include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>> n>>k;
+        int arr[n+1];
+        map<int, int>mp;
+        int res =0, vc =0, t=1;
+        for(int i=0;i<n;i++)
+        {
+            cin >> arr[i];
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     cout << arr[i];
+        // }
+        int column[n+1][n+1] = {0};
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                column[i][j]=0;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            mp.clear();
+            for(int j=i; j<n; j++)
+            {
+                column[i][j] = (j==0)?0:column[i][j-1];
+                if(mp.count(arr[j]))
+                {
+                    if(mp[arr[j]]==1)
+                    {
+                        column[i][j]++;
+                    }
+                    column[i][j]++;
+                }
+                mp[arr[j]]++;
+            }
+        }
+        res = 10000;
+        int flag =100;
+        int newarr[101][1001] = {0};
+        for(int i=0;i<=flag;i++)
+        {
+            for(int j=0;j<=flag;j++)
+            {
+                newarr[i][j]=0;
+            }
+        }
+        for(int i=0;i<n+1;i++)
+        {
+            newarr[1][i] = column[0][i-1];
+        }
+        for(int i=2;i<=flag;i++)
+        {
+            newarr[i][1]=0;
+        }
+        for(int i=2; i<=flag;i++)
+        {
+            for(int j=2;j<=n;j++)
+            {
+                int best = 10000;
+                for(int p=1; p<j;p++)
+                {
+                    best = min(best,newarr[i-1][p]+column[p][j-1]);
+                    //cout<<best
+                }
+                //cout<<best
+                newarr[i][j]=best;
+            }
+        }
+        for(flag=1;flag<=100;flag++)
+        {
+            res = min(flag*k+newarr[flag][n],res);
+            //cout<<res
+        }
+        cout<< res << endl;
+    }
+
+    return 0;
+}
+
